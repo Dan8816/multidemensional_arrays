f52ecf686f5dfa5f6d446100da958a23a8ff4adc
Dan8816 <dan881631537518@gmail.com>
2018-07-02 17:02:56 -0500

    1st commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace multi_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[10,10];
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("");
+                    for (int j = 0; j < 10; j++)
+                    {
+                        array2D[i,j] = (i+1)*(j+1);
+                        Console.WriteLine(array2D[i,j]);
+                    };
+                };
+            };
+        }
+    }
+}
